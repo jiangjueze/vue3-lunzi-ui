4329d52af15d02d6d60506153e4a594c09a2d738
jiangjueze <931914369@qq.com>
2021-09-15 15:31:04 +0800

    初始化vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>hi</div>
-  <Frank></Frank>
+  <div>
+  导航栏 |
+  <router-link to="/">Frank</router-link>|
+  <router-link to="/xxx">Frank2</router-link>
+  </div>
+  <hr/>
+  <router-view />
 </template>
 
 <script>
@@ -8,8 +13,5 @@ import Frank from './components/Frank.vue'
 
 export default {
   name: 'App',
-  components: {
-    Frank:Frank
-  }
 }
 </script>
