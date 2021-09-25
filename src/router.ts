fc55bb19432f8cb97af8c1e6ff75ef58bfc5a125
jiangjueze <931914369@qq.com>
2021-09-25 14:57:40 +0800

    error

--- src/router.ts
+++ src/router.ts
@@ -7,8 +7,10 @@ import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
 
-
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
 const history = createWebHashHistory()
+const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
 export const router = createRouter({
     history:history,
     routes: [
@@ -18,6 +20,9 @@ export const router = createRouter({
           component: Doc,
           children: [
               {path:'',component:DocDemo},
+              { path: "intro", component: md('intro') },
+              { path: "get-started", component: md('get-started') },
+              { path: "install", component: md('install') },
               { path: "switch", component: SwitchDemo },
               { path: "button", component: ButtonDemo },
               { path: "dialog", component: DialogDemo },
